4104980c4172d97198867555626b912b4d263c52
Kayla <6lackmacaroon@gmail.com>
2019-06-18 14:09:38 -0600

    created state and basic structure in app.js

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Display from './components/Display.js';
+import Dashboard from './components/Dashboard.js';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    player: "David Ortiz",
+    balls: 0,
+    strikes: 0
+  };
+
   render() {
       return (
         <div className="App">
-        
+          <header className='head'>
+            <Display />
+            <Dashboard />
+          </header>
+          
         </div>
       );
     }
